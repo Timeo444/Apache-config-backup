e40397a01bbea4144b36f180b6250cb278e00ce1
Ekerin Oluwatimileyin <omekerin@student.oauife.edu.ng>
2025-01-31 22:04:05 +0000

    Created an automated deployment script

---
+++ deploy_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pull latest config changes from GitHub
+cd ~/Git-Managed-Configuration/apache_config_backup
+git pull origin main
+
+# Copy updated config to Apache directory
+cp ~/Git-Managed-Configuration/apache_config_backup/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf
+
+# Restart Apache to apply changes
+sudo systemctl restart apache2
+
+echo "Apache configuration updated successfully!"
+
